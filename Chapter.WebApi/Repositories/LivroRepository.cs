94e7d7675e5c1df2e9b78f09da2cffa529e60cd3
EvertonMDS <emds404@gmail.com>
2022-09-28 15:17:15 -0300

    primeiro commit

---
+++ Chapter.WebApi/Repositories/LivroRepository.cs
@@ -0,0 +1,26 @@
+﻿using Chapter.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter.WebApi.Repositories
+{
+    public class LivroRepository
+    {
+        // retorna a lista de livros
+        public List<Livro> Listar()
+        {
+            var livros = new List<Livro>() 
+            {
+                new Livro() { Id = 1, Disponivel = true, QuantidadePaginas = 120, Titulo = "O poder do agora" },
+                new Livro() { Id = 2, Disponivel = false, QuantidadePaginas = 220, Titulo = "Do mil ao milhão" },
+                new Livro() { Id = 3, Disponivel = true, QuantidadePaginas = 220, Titulo = "Breves respostas para grandes questões" },
+                new Livro() { Id = 4, Disponivel = false, QuantidadePaginas = 220, Titulo = "Do mil ao milhão" },
+                new Livro() { Id = 5, Disponivel = false, QuantidadePaginas = 110, Titulo = "Teste de um livro" },
+                new Livro() { Id = 6, Disponivel = false, QuantidadePaginas = 1, Titulo = "Folha de papel" }
+            };
+            return livros;
+        }
+
+    }
+}
